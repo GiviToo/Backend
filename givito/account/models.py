a3eb8543ce86044cf714174de47ce9bca415508f
Alfian Isnan <fianshared@gmail.com>
2022-07-01 21:23:27 +0700

    Update minimum model

--- givito/account/models.py
+++ givito/account/models.py
@@ -1,3 +1,11 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    uuid = models.CharField(max_length=36, default=uuid4)
+    name = models.CharField(max_length=36)
+    phone = models.CharField(max_length=15, null=True, blank=True, default="")
+    email = models.EmailField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
